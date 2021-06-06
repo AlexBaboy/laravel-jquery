f3b30b72a7dc2f9ca3382e62b0e983b44dffede6
Alex Baboy <alexbaboy2016@gmail.com>
2021-06-06 23:01:29 +0300

    layouts fix

--- testProject/resources/views/register.blade.php
+++ testProject/resources/views/register.blade.php
@@ -10,11 +10,11 @@
     <form action="{{ route('user-form')}}" method="post">
         @csrf
         <div class="form-group">
-            <label for="name">Email*</label>
+            <label for="email">Email*</label>
             <input type="text" name="email" placeholder="Enter email" id="email" class="form-control">
         </div>
         <div class="form-group">
-            <label for="name">Phone</label>
+            <label for="phone">Phone</label>
             <input type="text" name="phone" placeholder="Enter phone" id="phone" class="form-control">
         </div>
         <div class="form-group">
@@ -22,19 +22,19 @@
             <input type="text" name="first-name" placeholder="Enter first name" id="first-name" class="form-control">
         </div>
         <div class="form-group">
-            <label for="first-name">Last Name*</label>
+            <label for="last-name">Last Name*</label>
             <input type="text" name="last-name" placeholder="Enter last name" id="last-name" class="form-control">
         </div>
         <div class="form-group">
-            <label for="first-name">Company Name</label>
-            <input type="text" name="company-name" placeholder="Enter company name" id="company name" class="form-control">
+            <label for="company-name">Company Name</label>
+            <input type="text" name="company-name" placeholder="Enter company name" id="company-name" class="form-control">
         </div>
         <div class="form-group">
-            <label for="first-name">Password*</label>
+            <label for="password">Password*</label>
             <input type="password" name="password" placeholder="Enter password" id="password" class="form-control">
         </div>
         <div class="form-group">
-            <label for="first-name">Confirm Password*</label>
+            <label for="confirm-password">Confirm Password*</label>
             <input type="password" name="confirm-password" placeholder="Confirm password" id="confirm-password" class="form-control">
         </div>
         <button type="submit" class="btn btn-success">register</button>
--- testProject/resources/views/update-user.blade.php
+++ testProject/resources/views/update-user.blade.php
@@ -8,29 +8,31 @@ Update
 <h1>Update</h1>
 
 <div>
-    @csrf
-    <div class="form-group">
-        <label for="name">Email</label>
-        <input type="text" name="email" placeholder="Enter email" id="email" value="{{$data->email}}" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="name">Phone</label>
-        <input type="text" name="phone" placeholder="Enter phone" id="phone" value="{{$data->phone}}" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="first-name">First Name</label>
-        <input type="text" name="first-name" placeholder="Enter first name" id="first-name" value="{{$data->first_name}}" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="last-name">Last Name</label>
-        <input type="text" name="last-name" placeholder="Enter last name" id="last-name" value="{{$data->last_name}}" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="company-name">Company Name</label>
-        <input type="text" name="company-name" placeholder="Enter company name" id="company name" value="{{$data->company_name}}" class="form-control">
-    </div>
+    <form id="user-form">
+        @csrf
+        <div class="form-group">
+            <label for="name">Email</label>
+            <input type="text" name="email" placeholder="Enter email" id="email" value="{{$data->email}}" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="name">Phone</label>
+            <input type="text" name="phone" placeholder="Enter phone" id="phone" value="{{$data->phone}}" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="first-name">First Name</label>
+            <input type="text" name="first-name" placeholder="Enter first name" id="first-name" value="{{$data->first_name}}" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="last-name">Last Name</label>
+            <input type="text" name="last-name" placeholder="Enter last name" id="last-name" value="{{$data->last_name}}" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="company-name">Company Name</label>
+            <input type="text" name="company-name" placeholder="Enter company name" id="company-name" value="{{$data->company_name}}" class="form-control">
+        </div>
 
-    <button class="btn btn-success">update</button>
+        <button ty class="btn btn-success">update</button>
+    </form>
 </div>
 
 
